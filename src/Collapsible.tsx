1d2d63bdc706ab8ea7e37d5301437b57b9e896ce
Quantum <quantum2048@gmail.com>
2024-04-07 02:19:40 -0400

    Fix Collapsible

--- src/Collapsible.tsx
+++ src/Collapsible.tsx
@@ -1,28 +1,18 @@
 import React from 'react';
 import {Collapse} from 'bootstrap';
-import classNames from 'classnames';
-
-export interface CollapsibleHandle {
-  show: () => void;
-  hide: () => void;
-  toggle: () => void;
-}
 
 export default function Collapsible({children, show}: { children: React.ReactNode; show: boolean }) {
-  const collapse = React.useRef<Collapse>();
-
-  const onLoad = React.useCallback((element: HTMLDivElement) => {
-    collapse.current = new Collapse(element, {toggle: show});
-  }, []);
+  const id = React.useId();
 
   React.useEffect(() => {
+    const collapse = Collapse.getOrCreateInstance(`#${id}`, {toggle: show});
     if (show)
-      collapse.current?.show();
+      collapse.show();
     else
-      collapse.current?.hide();
-  }, [show]);
+      collapse.hide();
+  }, [id, show]);
 
-  return <div ref={onLoad} className={classNames('collapse', {show})}>
+  return <div id={id} className="collapse">
     {children}
   </div>;
 }
