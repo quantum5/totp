c32f46a7f183347ca784ada242ecbde6b8491361
Quantum <me@quantum5.ca>
2025-02-10 12:17:47 -0500

    Add donation and footer

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,21 @@ import react from '@vitejs/plugin-react';
 import {nodePolyfills} from 'vite-plugin-node-polyfills';
 import svgr from 'vite-plugin-svgr';
 
+function yearPlugin() {
+  return {
+    name: 'year',
+    transformIndexHtml(html: string): string {
+      return html.replace('__YEAR__', new Date().getFullYear().toString());
+    },
+  } as const;
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svgr(),
     react(),
+    yearPlugin(),
     nodePolyfills({
       include: ['buffer'],
       globals: {Buffer: true},
